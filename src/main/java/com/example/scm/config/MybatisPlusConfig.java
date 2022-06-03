8c0190605d87becdb1b5ced513407c172492e53f
coming <1134382543@qq.com>
2022-06-04 00:51:09 +0800

    更新配置文件

--- src/main/java/com/example/scm/config/MybatisPlusConfig.java
+++ src/main/java/com/example/scm/config/MybatisPlusConfig.java
@@ -1,6 +1,8 @@
 package com.example.scm.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.core.MybatisConfiguration;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
@@ -27,11 +29,9 @@ import javax.sql.DataSource;
 @MapperScan("com.example.scm.dao")
 public class MybatisPlusConfig {
 
-
     @Bean("sqlSessionFactory")
     @Primary
     public SqlSessionFactory sqlSessionFactory(@Autowired @Qualifier("dataSource") DataSource dataSource) throws Exception {
-
         // 未使用MyBatis-Plus的分页插件的小伙伴可忽略此部分
         // MybatisPlus内部拦截器
         // 为自动分页插件设置DB类型
@@ -55,10 +55,17 @@ public class MybatisPlusConfig {
         // 添加MybatisConfiguration
         sqlSessionFactoryBean.setConfiguration(mybatisConfiguration);
 
-
         // 设置mapper相应的xml路径
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver()
                 .getResources("classpath:mapper/*.xml"));
+
+        // 主键自增
+        sqlSessionFactoryBean.getConfiguration().getGlobalConfig().getDbConfig().setIdType(IdType.AUTO);
+        // 逻辑删除位
+        sqlSessionFactoryBean.getConfiguration().getGlobalConfig().getDbConfig().setLogicDeleteField("state");
+        // 插入和修改策略
+        sqlSessionFactoryBean.getConfiguration().getGlobalConfig().getDbConfig().setUpdateStrategy(FieldStrategy.NOT_NULL);
+        sqlSessionFactoryBean.getConfiguration().getGlobalConfig().getDbConfig().setInsertStrategy(FieldStrategy.NOT_NULL);
 
         // 返回MybatisSqlSessionFactoryBean从而替代原生的sqlSessionFactory
         return sqlSessionFactoryBean.getObject();
